93edc09a288ec663f8ecbe52bb5aad8be03e1ed9
Chalana Shehara <chalanashehara19@gmail.com>
2023-03-26 23:03:43 +0530

    sign in button add

---
+++ lib/components/my_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatefulWidget {
+
+   MyButton({Key? key, required this.onTap}) : super(key: key);
+
+  final Function()? onTap;
+
+  @override
+  State<MyButton> createState() => _MyButtonState();
+}
+
+class _MyButtonState extends State<MyButton> {
+  @override
+  Widget build(BuildContext context) {
+    return  GestureDetector(
+      onTap: widget.onTap,
+      child: Container(
+        padding: EdgeInsets.all(15),
+        margin: EdgeInsets.symmetric(horizontal: 80),
+        decoration: BoxDecoration(color: Colors.indigo,
+        borderRadius: BorderRadius.circular(20)),
+        child: Center(
+
+         child: Text('Sign In',
+         style: TextStyle(color: Colors.black,
+         fontSize: 20,
+         fontWeight: FontWeight.bold),),
+
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:tiny_care/components/my_button.dart';
 import 'package:tiny_care/components/my_textfield.dart';
 import '../widgets/background_image.dart';
 
 class Root extends StatefulWidget {
   const Root({Key? key}) : super(key: key);
 
-  //Text Controller
 
 
 
   @override
   State<Root> createState() => _RootState();
 }
+
+//Text Controller
+
 final usernameController = TextEditingController();
 final passwordController = TextEditingController();
+//Sign User In
+
+void signUserIn(){}
 
 class _RootState extends State<Root> {
   @override
@@ -64,15 +70,93 @@ class _RootState extends State<Root> {
 
 
                     const SizedBox(height: 10,),
+
                     //Password
                     MyTextField(
                       controller : passwordController,
                       hintText: 'Password',
                       obscureText: true,
                     ),
-                    //
 
-                    //
+                    //Sign In Button
+                    const SizedBox(height: 10,),
+                    MyButton(
+                      onTap: (){},
+                    ),
+
+
+                    const SizedBox(height: 1,),
+
+                    // forgot button
+                    Row(
+                      children: [
+                        const SizedBox(width: 20,),
+                        Column(
+                          children: [
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                textStyle: const TextStyle(fontSize: 13),
+                              ),
+                              onPressed: (){},
+                              child: const Text('Forgot Password?',
+                              style: TextStyle(
+                                color: Colors.black
+                              ),),
+                            ),
+                          ],
+                        ),
+
+                        const SizedBox(width: 30,),
+                        Column(
+                          children: [
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                textStyle: const TextStyle(fontSize: 13),
+
+                              ),
+                              onPressed: (){},
+                              child: const Text('Create New Account', style: TextStyle(
+                                color: Colors.black
+                              ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+
+                    //or Sign In button
+                    const SizedBox(height: 4,),
+
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      child: Row(
+                        children: const [
+                          Expanded(
+                            child: Divider(
+                              thickness: 0.5,
+                              color: Colors.black,
+                            ),
+                          ),
+
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 10),
+                            child: Text('or',
+                            style: TextStyle(
+                              color: Colors.black
+                            ),),
+                          ),
+
+                          Expanded(
+                            child: Divider(
+                              thickness: 1.5,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
                   ],
                 ),
               ),
